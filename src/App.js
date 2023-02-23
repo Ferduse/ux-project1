4b72b70b6eec276cfc474784c6c968260fdbdd6c
Ferduse <ferduse.rahman79@myhunter.cuny.edu>
2023-02-23 15:54:07 -0500

    Added elements, path, and navigation to App.jss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Plants from './components/Plants';
+import About from './components/About';
+
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='menu' element={<Menu />}/>
+          <Route path='plants' element={<Plants />}/>
+          <Route path='about' element={<About />}/>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+const navStyle = {textDecoration: "none", color:"pink"};
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link style={navStyle} to="/">Home</Link>
+          </li>
+          <li>
+            <Link style={navStyle} to="/About">About</Link>
+          </li>
+          <li>
+            <Link style={navStyle} to="/Menu">Menu</Link>
+          </li>
+          <li>
+            <Link style={navStyle} to="/Plants">Plants</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
